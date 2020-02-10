ccb3341f4633bb95af6c5023c12590cc9d96ee55
dev-bot1 <kshitij_201900328@smit.smu.edu.in>
2020-02-10 10:08:25 +0530

    Final commit allowed by Avi

---
+++ Application Code/Splash screen and Login Page/app/src/main/java/com/example/splashscreen/mess_login.java
@@ -0,0 +1,19 @@
+package com.example.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mess_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mess_login);
+
+    }
+    //public void btn_login(View view) {
+    //startActivity(new Intent(getApplicationContext(),//utkarsh firstpg));
+
+    //}
+}
